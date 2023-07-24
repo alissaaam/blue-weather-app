579ad0c521f331677c0a39665e471c5df2269545
alissaaam <aalissa16m@gmail.com>
2023-07-24 19:18:46 -0400

    Created the api

--- script/weather.js
+++ script/weather.js
@@ -36,7 +36,8 @@ if (minutes < 10) {
 }
 h2.innerHTML = `${day} ${month} ${date}, ${hours}:${minutes} PM `;
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#weather-forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -62,6 +63,14 @@ function displayForecast() {
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "dcb289ab0182fef55a284bc68b9e0fff";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function showWeather(response) {
@@ -128,4 +137,3 @@ let celsiusLink = document.querySelector("#celsius-unit");
 celsiusLink.addEventListener("click", giveCelsius);
 
 search("Florida");
-displayForecast();
