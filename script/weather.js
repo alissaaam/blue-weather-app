979a8d3e0da8f74a19530f6137c7d4d8da4cd632
alissaaam <aalissa16m@gmail.com>
2023-07-25 12:15:41 -0400

    Updated html, css, and js files

--- script/weather.js
+++ script/weather.js
@@ -56,14 +56,15 @@ function displayForecast(response) {
         forecastHTML +
         `
         <div class="col-2">
-        <img
+        
+          <div class="weather-date">${formatDay(forecastDay.dt)}</div>
+          <img
             src=" https://openweathermap.org/img/wn/${
               forecastDay.weather[0].icon
             }@2x.png"
             alt=""
             width="42"
           />
-          <div class="weather-date">${formatDay(forecastDay.dt)}</div>
           <div class="weather-temperatures">
             <span class="weather-forecast-max"> ${Math.round(
               forecastDay.temp.max
@@ -82,8 +83,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
-  let apiKey = "dcb289ab0182fef55a284bc68b9e0fff";
+  let apiKey = "50fa4024e3b1d5eac2f51ab18a47e997";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -111,7 +111,7 @@ function showWeather(response) {
 }
 
 function search(city) {
-  let apiKey = "dcb289ab0182fef55a284bc68b9e0fff";
+  let apiKey = "50fa4024e3b1d5eac2f51ab18a47e997";
   let units = "metric";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
   axios.get(apiUrl).then(showWeather);
